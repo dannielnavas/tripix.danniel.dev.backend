a0073f6c4308f74c369dedb4db25c2d00bd97e04
Danniel Navas <dannielnavas@gmail.com>
2025-05-06 21:22:12 -0500

    feat: update user schema to include role validations, tokens and recoveryToken

--- api/schemas/user.schema.js
+++ api/schemas/user.schema.js
@@ -3,12 +3,16 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const email = Joi.string().email();
 const name = Joi.string().min(3);
-const role = Joi.string().min(5);
+const role = Joi.string().min(4).max(5).valid('free', 'premium', 'admin');
+const token = Joi.number().integer().min(0).max(4);
+const recoveryToken = Joi.string().min(10).max(100);
 
 const createUserSchema = Joi.object({
   email: email.required(),
   name: name.required(),
   role: role.required(),
+  tokens: token.required(),
+  recoveryToken: recoveryToken,
 });
 
 const updateUserSchema = Joi.object({
